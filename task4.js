9c4d34ea9518debf1259f9588f3d74f0f46751cd
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-04-29 00:29:40 +0500

    Fix eslint linter error

--- task4.js
+++ task4.js
@@ -3,9 +3,8 @@ function capitalize(string) {
     const str = string.split('');
     str[0] = str[0].toUpperCase();
     return str.join('');
-  } else {
-    throw new Error('An error occurred.')
   }
+  throw new Error('An error occurred.');
 }
 
 module.exports = { capitalize };
